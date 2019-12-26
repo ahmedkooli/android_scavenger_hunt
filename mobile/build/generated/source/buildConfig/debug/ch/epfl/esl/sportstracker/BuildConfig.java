3b8eebc3253490b2f29e027166ad39c8697a7e99
ahmedkooli <56259512+ahmedkooli@users.noreply.github.com>
2019-12-26 21:10:26 +0100

    Add files via upload

---
+++ mobile/build/generated/source/buildConfig/debug/ch/epfl/esl/sportstracker/BuildConfig.java
@@ -0,0 +1,24 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ch.epfl.esl.sportstracker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ch.epfl.esl.sportstracker";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String W_a_key = "a_value";
+  public static final String W_example_path_asset = "/ ASSET ";
+  public static final String W_example_path_datamap = "/DATAMAP";
+  public static final String W_example_path_text = "/TEXT";
+  public static final String W_mainactivity = "MainActivity";
+  public static final String W_path_acknowledge = "/ACKNOWLEDGE";
+  public static final String W_path_start_activity = "/START_ACTIVITY";
+  public static final String W_profile_key = "profile_key";
+  public static final String W_profile_path = "/PROFILE";
+  public static final String W_some_other_key = "some_other_value";
+}
